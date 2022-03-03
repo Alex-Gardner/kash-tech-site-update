8a1213cf2f2e4e05b221bef84f518fcfaa10878c
devlex <a.gardner.g@gmail.com>
2022-03-03 16:17:16 -0500

    basic styling of header, hero, and who we are section

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Header template to be appended
 
-const template = document.createElement('template');
+// const template = document.createElement('template');
 
-template.innerHTML = `
-    <header class="component-site-header">
-        <div class="logo-icon-holder"></div>
-        <div class="nav-and-contact-holder">
-            <nav></nav>
-        </div>
-    </header>
-`
-document.body.appendChild(template.content);
+// template.innerHTML = `
+//     <header class="component-site-header">
+//         <div class="logo-icon-holder"></div>
+//         <div class="nav-and-contact-holder">
+//             <nav></nav>
+//         </div>
+//     </header>
+// `
+// document.body.appendChild(template.content);
